0cc4f6a1ee1437faa4bce95f8ab6d73c43edffbe
Alipheese16th <117807553+Alipheese16th@users.noreply.github.com>
2025-04-03 13:57:12 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 180 ms, Memory: 20732 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cs
@@ -0,0 +1,34 @@
+StreamReader sr = new(Console.OpenStandardInput());
+StreamWriter sw = new(Console.OpenStandardOutput());
+
+try
+{
+
+    int N = int.Parse(sr.ReadLine());
+
+    List<(int x, int y)> list = new();
+    // ValueTuple 타입의 리스트
+
+    for (int i = 0; i < N; i++)
+    {
+        string[] input = sr.ReadLine().Split();
+        list.Add((int.Parse(input[0]), int.Parse(input[1])));
+        
+    }
+
+    list.Sort();
+    list.ForEach(item=>sw.WriteLine(item.x + " " + item.y));
+
+
+
+
+}
+catch (Exception ex)
+{
+    sw.WriteLine(ex.Message);
+}
+finally
+{
+    sr.Close();
+    sw.Close();
+}
