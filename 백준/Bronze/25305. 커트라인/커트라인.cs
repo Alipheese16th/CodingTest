a735c92371f0d5356915ed34f89ce80f139e45a0
Alipheese16th <117807553+Alipheese16th@users.noreply.github.com>
2025-04-15 13:08:44 +0900

    [Bronze II] Title: 커트라인, Time: 52 ms, Memory: 6064 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cs
@@ -0,0 +1,21 @@
+
+string[] input = Console.ReadLine().Split();
+
+int n = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+
+List<int> intList = new();
+
+string[] inputSecond = Console.ReadLine().Split();
+
+
+for (int i = 0; i < n; i++)
+{
+    intList.Add(int.Parse(inputSecond[i]));
+}
+
+intList.Sort((a, b) => b.CompareTo(a));
+
+Console.WriteLine(intList[k-1]);
+
+
