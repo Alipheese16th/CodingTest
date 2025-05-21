742c285d6daec690cc17587732723ef653473058
Alipheese16th <117807553+Alipheese16th@users.noreply.github.com>
2025-05-21 19:03:52 +0900

    [Bronze I] Title: 약수, Time: 80 ms, Memory: 5744 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cs
@@ -0,0 +1,20 @@
+
+using System.Text;
+
+StreamReader sr = new(Console.OpenStandardInput());
+StreamWriter sw = new(Console.OpenStandardOutput());
+
+
+int N = int.Parse(sr.ReadLine() ?? "");
+
+int min, max;
+
+List<int> list = (sr.ReadLine() ?? "").Split().Select(int.Parse).ToList();
+min = list.Min();
+max = list.Max();
+
+sw.Write(min * max);
+
+
+sr.Close();
+sw.Close();
