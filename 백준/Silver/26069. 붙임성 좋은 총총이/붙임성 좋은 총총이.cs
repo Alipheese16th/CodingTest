9bd87dd9d0d7624c1a702d1bd49c6d3e6a74cdba
Alipheese16th <117807553+Alipheese16th@users.noreply.github.com>
2025-05-23 12:28:07 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 52 ms, Memory: 6032 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.cs
@@ -0,0 +1,32 @@
+StreamReader sr = new(Console.OpenStandardInput());
+StreamWriter sw = new(Console.OpenStandardOutput());
+
+
+int N = int.Parse(sr.ReadLine() ?? "");
+HashSet<string> set = new HashSet<string>();
+int result = 0;
+
+set.Add("ChongChong");
+
+for (int i = 0; i < N; i++)
+{
+    string[] input = (sr.ReadLine() ?? "").Split();
+
+    if (set.Contains(input[0])) {
+        set.Add(input[1]);
+    } else if (set.Contains(input[1])) {
+        set.Add(input[0]);
+    }
+
+
+}
+result += set.Count;
+
+sw.Write(result);
+
+
+
+
+
+sr.Close();
+sw.Close();
